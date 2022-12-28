8ee2a881c9fc067f0df7f9ab324384ad5e169474
Darshan M <darshanmukunda098@gmail.com>
2022-12-28 13:55:48 +0530

    simple todos list

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,71 +1,42 @@
 <script setup>
-import { ref, reactive } from "vue";
-
-const message = ref("Hello World");
-message.value = "Changed";
-const count = ref(0);
-const counter = reactive({
-  count: 0,
-});
-
-const text = ref("");
-const awesome = ref(true);
-
-count.value++;
-counter.count++;
-
+import { ref, computed } from "vue";
 let id = 0;
 
 const newTodo = ref("");
+const hideCompleted = ref(false);
 const todos = ref([
-  { id: id++, text: "Learn HTML" },
-  { id: id++, text: "Learn JavaScript" },
-  { id: id++, text: "Learn Vue" },
+  { id: id++, text: "Learn HTML", done: true },
+  { id: id++, text: "Learn JavaScript", done: true },
+  { id: id++, text: "Learn Vue", done: false },
 ]);
 
-function increment() {
-  ++count.value;
-}
-function toggle() {
-  awesome.value = !awesome.value;
-}
-function onInput(e) {
-  text.value = e.target.value;
-}
+const filteredTodos = computed(() => {
+  return hideCompleted.value 
+  ? todos.value.filter(t=>!t.done) 
+  : todos.value;
+});
+
 function addTodo() {
-  todos.value.push({ id: id++, text: newTodo.value });
+  todos.value.push({ id: id++, text: newTodo.value, done: false });
   newTodo.value = "";
 }
 function removeTodo(todo) {
-  todos.value = todos.value.filter((t)=>t!==todo)
+  todos.value = todos.value.filter((t) => t !== todo);
 }
-//while(true){counter.count++; console.log(counter.count);break;}
-//component logic
-//declare some reactive state here
 </script>
 <template>
-  <h1>{{ message }}</h1>
-  <p>Count is: {{ counter.count }}</p>
-  <h1 :class="counter">{{ counter.count }}</h1>
-  <h1 @click="increment">{{ count }}</h1>
-
-  <div><input :value="text" @input="onInput" placeholder="Type Here" /></div>
-
-  <div><input v-model="text" /></div>
-  <p>{{ text }}</p>
-  <button @click="toggle">toggle</button>
-  <h1 v-if="awesome">Vue is awesome</h1>
-  <h1 v-else>Oh no 😢</h1>
-  <h1>Make me dynamic</h1>
-
   <form @submit.prevent="addTodo">
     <input v-model="newTodo" />
     <button>Add Todo</button>
   </form>
   <ul>
-    <li v-for="todo in todos" :key="todo.id">
-      {{ todo.text }}
+    <li v-for="todo in filteredTodos" :key="todo.id">
+      <input type="checkbox" v-model="todo.done" />
+      <span :class="{ done: todo.done }">{{ todo.text }}</span>
       <button @click="removeTodo(todo)">X</button>
     </li>
   </ul>
+  <button @click="hideCompleted = !hideCompleted">
+    {{ hideCompleted ? "Show All" : "Hide Completed" }}
+  </button>
 </template>
