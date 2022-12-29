b9801e6f2904aa61a04eec0cb38ac1021cfc94d3
Darshan M <darshanmukunda098@gmail.com>
2022-12-30 00:55:49 +0530

    ex4 Conditionals and Loops

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,35 +1,18 @@
 <script setup>
-import { ref } from "vue";
+import {ref} from 'vue'
 
-const message = ref("Hello World");
-const isRed = ref(true);
-const color = ref("green");
-
-function toggleRed() {
-  isRed.value = !isRed.value;
-}
-
-function toggleColor() {
-  color.value = color.value === "green" ? "blue" : "green";
-}
+const show = ref(true)
+const list = ref([1,2,3])
 </script>
 <template>
-  <p>
-    <span :title="message">
-      Hover your mouse over for me a few seconds to see my dynamically bound
-      title!
-    </span>
-  </p>
-  <p :class="{ red: isRed }" @click="toggleRed">
-    This should be red... but click me to toggle it.
-  </p>
-  <p :style="{ color }" @click="toggleColor">
-    This should be green, and should toggle between green and blue on click
-  </p>
+  <button @click="show = !show">Toggle List</button>
+  <button @click="list.push(list.length + 1)">Push Number</button>
+  <button @click="list.pop()">Pop Number</button>
+  <button @click="list.reverse()">Reverse List</button>
 
+  <ul v-if="show && list.length">
+    <li v-for="item of list">{{ item }}</li>
+  </ul>
+  <p v-else-if="list.length">List is not empty, but hidden </p>
+  <p v-else>List is empty</p>
 </template>
-<style>
-.red{
-  color: red;
-}
-</style>
