b1e8c795712b79f122484d29682fb67cae16b737
Darshan M <darshanmukunda098@gmail.com>
2022-12-30 14:16:58 +0530

    ADD editable todo title field

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,15 +1,16 @@
 <script setup>
-import { ref, computed, watchEffect } from "vue";
+import { ref, computed, watchEffect } from 'vue';
 
 let id = 0;
 
-const newTodo = ref("");
 const hideCompleted = ref(false);
 
-const todos = ref(JSON.parse(localStorage.getItem("todos") || "[]"));
+const todos = ref(JSON.parse(localStorage.getItem('todos') || '[]'));
+
 watchEffect(() => {
-  localStorage.setItem("todos", JSON.stringify(todos.value));
+  localStorage.setItem('todos', JSON.stringify(todos.value));
 });
+
 const filteredTodos = computed(() => {
   return hideCompleted.value ? todos.value.filter((t) => !t.done) : todos.value;
 });
@@ -20,39 +21,41 @@ function addTodo(e) {
     todos.value.push({
       id: id++,
       text: value,
-      done: false,
+      done: false
     });
-  e.target.value = "";
+  e.target.value = '';
 }
-
 
 function removeTodo(todo) {
-  todos.value.splice(todos.value.indexOf(todo), 1)
+  todos.value.splice(todos.value.indexOf(todo), 1);
 }
+
 function editTodo(todo) {
-  
-  todo.text = todo.text
+  todo.text = todo.text;
 }
 </script>
 
 <template>
   <h1>todos</h1>
-  
+
   <input
     class="new-todo"
     autofocus
     placeholder="What needs to be done?"
     @keyup.enter="addTodo"
   />
+
   <ul>
     <li v-for="todo in filteredTodos" :key="todo.id">
       <input type="checkbox" v-model="todo.done" />
-      <span :class="{ done: todo.done }" @dblclick="editTodo(todo)">{{ todo.text }}</span>
+      <input :class="{ done: todo.done }" @dblclick="editTodo(todo)" :value="todo.text"><!-- {{
+        todo.text
+      }}</input> -->
       <button @click="removeTodo(todo)">X</button>
     </li>
   </ul>
   <button @click="hideCompleted = !hideCompleted">
-    {{ hideCompleted ? "Show all" : "Hide completed" }}
+    {{ hideCompleted ? 'Show all' : 'Hide completed' }}
   </button>
 </template>
 
