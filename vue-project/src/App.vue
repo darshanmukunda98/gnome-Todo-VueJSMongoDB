fb4eb7904953b03fcd652d803a4bd9bb79f4dc01
Darshan M <darshanmukunda098@gmail.com>
2022-12-30 10:59:11 +0530

    ADD todos array live storage

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,8 +1,18 @@
 <script setup>
-import {ref} from 'vue'
+import { ref } from "vue";
 
-function addTodo(e){
-  console.log(e.target.value)
+const todos = ref([]);
+function addTodo(e) {
+  const value = e.target.value;
+  if (value) {
+    todos.value.push({
+      id: Date.now(),
+      title: value,
+      completed: false,
+    });
+    e.target.value = "";
+  }
+  console.log(todos.value)
 }
 </script>
 <template>
@@ -10,11 +20,11 @@ function addTodo(e){
     <header class="header">
       <h1>todos</h1>
       <input
-      class="new-todo"
-      autofocus
-      placeholder="What needs to be done?"
-      @keyup.enter="addTodo"
-      >
+        class="new-todo"
+        autofocus
+        placeholder="What needs to be done?"
+        @keyup.enter="addTodo"
+      />
     </header>
   </section>
 </template>
