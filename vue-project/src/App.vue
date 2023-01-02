df3a87589ecceba58101e6f002399998d1f55863
Darshan M <darshanmukunda098@gmail.com>
2023-01-02 11:17:12 +0530

    ADD input field, app header, todo-list rendering

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,66 +1,47 @@
 <script setup>
-import { ref, computed, watchEffect } from 'vue';
+import { ref, watch } from "vue";
+let i = 0;
+let todo = ref("");
+let todos = ref([]);
+/* watch(todo,(t)=>{
+  localStorage.setItem("data",JSON.stringify(t))
+}) */
 
-let id = 0;
-
-const hideCompleted = ref(false);
-
-const todos = ref(JSON.parse(localStorage.getItem('todos') || '[]'));
-
-watchEffect(() => {
-  localStorage.setItem('todos', JSON.stringify(todos.value));
-});
-
-const filteredTodos = computed(() => {
-  return hideCompleted.value ? todos.value.filter((t) => !t.done) : todos.value;
-});
-
-function addTodo(e) {
-  const value = e.target.value;
-  if (value)
-    todos.value.push({
-      id: id++,
-      text: value,
-      done: false
-    });
-  e.target.value = '';
+function onInput(e) {
+  console.log("TITLE " + e.target.value);
+  let title = e.target.value;
+  e.target.value = "";
+  todos.value.push({
+    id: Date.now(),
+    title: title,
+    done: false,
+  });
+  console.log("TODOS");
+  console.log(todos.value);
+  localStorage.setItem("data", JSON.stringify(todos.value));
 }
-
-function removeTodo(todo) {
-  todos.value.splice(todos.value.indexOf(todo), 1);
-}
-
-function editTodo(todo) {
-  todo.text = todo.text;
+function toggle(e) {
+  console.log(e.target.parentNode.parentNode.id);
+  console.log(e.target.checked);
 }
 </script>
-
 <template>
+  <!--<form @submit.prevent="onInput" >{{ todo }}</form> read on v-bind vs v-model -->
+  <div class="app">
   <h1>todos</h1>
-
-  <input
-    class="new-todo"
-    autofocus
-    placeholder="What needs to be done?"
-    @keyup.enter="addTodo"
-  />
-
-  <ul>
-    <li v-for="todo in filteredTodos" :key="todo.id">
-      <input type="checkbox" v-model="todo.done" />
-      <input :class="{ done: todo.done }" @dblclick="editTodo(todo)" :value="todo.text"><!-- {{
-        todo.text
-      }}</input> -->
-      <button @click="removeTodo(todo)">X</button>
-    </li>
-  </ul>
-  <button @click="hideCompleted = !hideCompleted">
-    {{ hideCompleted ? 'Show all' : 'Hide completed' }}
-  </button>
+  <input @keydown.enter="onInput" />{{ todo }}
+  <div v-for="item in todos" :key="item.id">
+    <div class="todo--item" :id="item.id">
+      <span>
+        <input
+          type="checkbox"
+          @change="toggle" 
+        />
+        <input v-model="item.title" />
+      </span>
+    </div>
+  </div>
+</div>
 </template>
-
 <style>
-.done {
-  text-decoration: line-through;
-}
 </style>
