d4cfeb370aac06c1029aa722b0002b42fdd6facb
Darshan M <darshanmukunda098@gmail.com>
2023-01-03 16:21:41 +0530

    add expand func, del, notes, priority, date button

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,7 +2,12 @@
 import { ref } from 'vue';
 
 let todos = ref(JSON.parse(localStorage.getItem('data') || '[]'));
-let checkAllDone = todos.value.every((todo) => todo.done === true);
+let checkAllDone;
+
+checkAllDone =
+  todos.value.length === 0
+    ? false
+    : (checkAllDone = todos.value.every((todo) => todo.done === true));
 console.log(checkAllDone);
 
 function onInput(e) {
@@ -45,42 +50,82 @@ function allDone(e) {
     <div class="list">
       <span class="todo--input">
         <input type="checkbox" @change="allDone" v-model="checkAllDone" />
-        <input @keydown.enter="onInput" class="todo--title--input" autofocus placeholder="Whats needs to be done?"/>
+        <input
+          @keydown.enter="onInput"
+          class="todo--title--input"
+          autofocus
+          placeholder="Whats needs to be done?"
+        />
       </span>
       <div v-for="(item, index) in todos" :key="item.id">
-        <div class="todo--item" :id="item.id" @change="onChange">
-          <input
-            class="todo--item--checkbox"
-            type="checkbox"
-            name="checkbox"
-            v-model="todos[index].done"
-          />
-          <input
-            class="todo--item--title"
-            v-model="todos[index].title"
-            name="title"
-          />
-        </div>
+        <details class="todo--item" :id="item.id" @change="onChange">
+          <summary style="display: block">
+            <input
+              class="todo--item--checkbox"
+              type="checkbox"
+              name="checkbox"
+              v-model="todos[index].done"
+            />
+            <input
+              class="todo--item--title"
+              v-model="todos[index].title"
+              name="title"
+            />
+            <span>▼</span>
+            <input type="button" value="X" />
+          </summary>
+          <label>Notes</label>
+          <input class="todo--item--notes" type="textarea" />
+          <label>Date</label>
+          <input type="date" />
+          <label>Priority</label>
+          <select>
+            <option disabled selected>None</option>
+            <option value="high">High</option>
+            <option value="medium">Medium</option>
+            <option value="low">Low</option>
+          </select>
+        </details>
       </div>
     </div>
   </div>
 </template>
 <style scoped>
+.app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: auto;
+}
+.app h1 {
+  font-size: 100px;
+  font-weight: 100;
+  font-family: Arial, Helvetica, sans-serif;
+  color: red;
+}
+.list {
+  border: 1px solid grey;
+  box-shadow: 5px 5px grey;
+  width: 700px;
+}
+
 .todo--input {
   display: flex;
   height: 40px;
   width: 100%;
   padding: 5px 0;
   border-style: solid;
-  border-width:1px;
-  border-color: grey;
+  border-width: 1px;
+  border-color: rgb(190, 190, 190);
 }
 
 .todo--title--input {
   width: 100%;
-  border-style:solid;
-  border-width:1px;
-  font-size: x-large;
+  border-style: solid;
+  border-width: 1px;
+  font-size: large;
+  font-style: italic;
 }
 .todo--item {
   display: flex;
@@ -88,11 +133,19 @@ function allDone(e) {
   padding: 5px 0;
   border-style: solid;
   border-width: 1px;
-  border-color: grey;
+  border-color: rgb(190, 190, 190);
+  height: auto;
+  flex-direction: row;
+}
+.todo--item--checkbox {
+  content: '';
+}
+.todo--item--checkbox:checked {
+  content: '✓';
 }
 .todo--item--title {
-  width: 100%;
+  width: 90%;
   border-style: none;
-  font-size: x-large;
+  font-size: large;
 }
 </style>
