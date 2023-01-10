e79ca5f1475857e426ba40e4be407ad6ffdb6dfb
Darshan M <darshanmukunda098@gmail.com>
2023-01-10 13:27:13 +0530

    add updateOne

--- REST_API/server.js
+++ REST_API/server.js
@@ -23,10 +23,22 @@ const result = await collection.insertOne(doc);
 console.log(
    `A document was inserted with the _id: ${result.insertedId}`,
 ); */
-const find_result = await collection.find({})
+/* const find_result = await collection.find({})
 await find_result.forEach(element => {
   console.log(element)
-})
+}) */
+const filter = { title: "todo 2" }
+const updateDoc = {
+  $set: {
+    notes: "notes 2"
+  },
+};
+const result = await collection.updateOne(filter, updateDoc);
+    console.log(
+      result
+    );
+/* const find_result = await collection.findOne({ _id: ObjectId("63bd1604166c68e8f13d3d67") })
+console.log(find_result) */
   return '.done'
 } 
 
