b32f5e55c544e59657587691ea92fd63f8d205ac
Darshan M <darshanmukunda098@gmail.com>
2023-01-10 12:13:09 +0530

    fix error url 'localhost' to 127.0.0.1

--- REST_API/server.js
+++ REST_API/server.js
@@ -1,31 +1,19 @@
-const {MongoClient} = require('mongodb');
-async function main(){
-  
-  const uri = 'mongodb://localhost:27017';
+const { MongoClient } = require('mongodb');
 
+const url = 'mongodb://127.0.0.1:27017/';
+const client = new MongoClient(url);
 
-  const client = new MongoClient(uri);
-  console.log(client)
+const dbName = 'todos';
 
-  try {
-      // Connect to the MongoDB cluster
-      await client.connect();
-
-      // Make the appropriate DB calls
-      await  listDatabases(client);
-
-  } catch (e) {
-      console.error(e);
-  } finally {
-      await client.close();
-  }
+async function main() {
+  await client.connect();
+  console.log('Connected successfully to server');
+  const db = client.db(dbName);
+  const collection = db.collection('todos');
+  return '.done'
 }
 
-main().catch(console.error);
-
-async function listDatabases(client){
-  databasesList = await client.db().admin().listDatabases();
-
-  console.log("Databases:");
-  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
-};
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
