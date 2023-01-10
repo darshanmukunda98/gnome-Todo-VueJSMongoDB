cbcd71fbaa97c5aa6112e17366dee1ad3e6d0009
Darshan M <darshanmukunda098@gmail.com>
2023-01-10 17:19:15 +0530

    hello world express.js

--- REST_API/server.js
+++ REST_API/server.js
@@ -1,4 +1,15 @@
 import { connDB, getAllTodos, insertOneTodo } from './database.js';
+import express  from 'express';
+
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 
 connDB();
 
@@ -6,16 +17,16 @@ const getTodos = async () => {
   let result = await getAllTodos();
   console.log(result);
 };
-const insertTodo = async () =>{
-    const todo = {
-        title: 'todo 3',
-        done: false,
-        notes: '',
-        date: '',
-        priority: 'none',
-        deleted: false
-      }
-    await insertOneTodo(todo)
-}
+const insertTodo = async () => {
+  const todo = {
+    title: 'todo 3',
+    done: false,
+    notes: '',
+    date: '',
+    priority: 'none',
+    deleted: false
+  };
+  await insertOneTodo(todo);
+};
 //getTodos();
-insertTodo()
+insertTodo();
