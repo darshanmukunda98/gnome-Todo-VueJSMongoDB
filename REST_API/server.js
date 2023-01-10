20ddf65e62da24341b50943f18c669cde2a14c44
Darshan M <darshanmukunda098@gmail.com>
2023-01-10 13:34:04 +0530

    add updateMany

--- REST_API/server.js
+++ REST_API/server.js
@@ -1,6 +1,5 @@
 const { MongoClient, ObjectId } = require('mongodb');
 
-
 const url = 'mongodb://127.0.0.1:27017/';
 const client = new MongoClient(url);
 
@@ -11,7 +10,7 @@ async function main() {
   console.log('Connected successfully to server');
   const db = client.db(dbName);
   const collection = db.collection('todos');
-/*   const doc = {
+  /*   const doc = {
     title: 'todo 3',
     done: false,
     notes: '',
@@ -23,11 +22,11 @@ const result = await collection.insertOne(doc);
 console.log(
    `A document was inserted with the _id: ${result.insertedId}`,
 ); */
-/* const find_result = await collection.find({})
+  /* const find_result = await collection.find({})
 await find_result.forEach(element => {
   console.log(element)
 }) */
-const filter = { title: "todo 3" }
+  /* const filter = { title: "todo 3" }
 const updateDoc = {
   $set: {
     notes: "notes 3"
@@ -36,11 +35,15 @@ const updateDoc = {
 const result = await collection.findOneAndUpdate(filter, updateDoc);
     console.log(
       result
-    );
-/* const find_result = await collection.findOne({ _id: ObjectId("63bd1604166c68e8f13d3d67") })
+    ); */
+  const filter = {};
+  const updateDoc = { $set: { done: true } };
+  const result = await collection.updateMany(filter, updateDoc);
+  console.log(result);
+  /* const find_result = await collection.findOne({ _id: ObjectId("63bd1604166c68e8f13d3d67") })
 console.log(find_result) */
-  return '.done'
-} 
+  return '.done';
+}
 
 main()
   .then(console.log)
