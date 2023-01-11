b9f4a1b8b917c7cae1899d7699dc708b9b4bb1f5
Darshan M <darshanmukunda098@gmail.com>
2023-01-11 16:49:59 +0530

    add fetch updateTodo

--- vue-project/requests.js
+++ vue-project/requests.js
@@ -14,8 +14,28 @@ async function insertTodo(todo) {
   ).json();
   console.log(response);
 }
+async function updateTodo(id, body) {
+  let response = await (
+    await fetch('http://localhost:3000/update/' + id, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify(body)
+    })
+  ).json();
+  console.log(response);
+}
 //fetchAllTodos();
-insertTodo({
+/* insertTodo({
+  title: 'Todo 11',
+  done: true,
+  notes: '',
+  date: '',
+  priority: 'none',
+  deleted: false
+}); */
+updateTodo('63be93baf728db150ff01855', {
   title: 'Todo 11',
   done: true,
   notes: '',
