a30dc3dc80daa94390dfd9f337bdce4379d8bb07
Darshan M <darshanmukunda098@gmail.com>
2023-01-11 10:54:12 +0530

    added PUT and DEL routes

--- REST_API/server.js
+++ REST_API/server.js
@@ -1,25 +1,34 @@
 import express from 'express';
 import { getAllTodos, insertOneTodo } from './model/database.js';
+
 const app = express();
-app.use(express.json())
+app.use(express.json());
+
 app.get('/', async (req, res) => {
-    try{
-        const todos = await getAllTodos()
-        res.json(todos)
-    }catch(err)
-    {
-        console.log(err)
-    }
+  try {
+    const todos = await getAllTodos();
+    res.json(todos);
+  } catch (err) {
+    console.log(err);
+  }
 });
+
 app.post('/insert', async (req, res) => {
-    try{
-        console.log(req.body)
-        const todo = await insertOneTodo(req.body)
-        res.json(todo)
-    }catch(err)
-    {
-        console.log(err)
-    }
+  try {
+    console.log(req.body);
+    const todo = await insertOneTodo(req.body);
+    res.json(todo);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.put('/user', (req, res) => {
+  res.send('Got a PUT request at /user');
+});
+
+app.delete('/user', (req, res) => {
+  res.send('Got a DELETE request at /user');
 });
 
 app.listen(3000, () => {
